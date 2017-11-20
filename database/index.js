807c688df153b64457f50d1e988cd96a3adaabfe
Adam Miller <chadam189@gmail.com>
2017-11-19 17:40:10 -0800

    Update database index.js file

--- database/index.js
+++ database/index.js
@@ -1,9 +1,18 @@
-const { Client } = require('pg');
-const client = new Client();
+// const { Client } = require('pg');
+// const client = new Client();
 
+// client.connect();
+
+// client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+//   console.log(err ? err.stack : res.rows[0].message); // Hello World!
+//   client.end();
+// });
+
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+
+const client = new pg.Client(connectionString);
 client.connect();
-
-client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
-  console.log(err ? err.stack : res.rows[0].message); // Hello World!
-  client.end();
-});
+const query = client.query(
+  'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+query.on('end', () => { client.end(); });
