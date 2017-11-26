f598e32b999895f84460a836d0dc88f41ee12e5c
Adam Miller <chadam189@gmail.com>
2017-11-25 16:01:37 -0800

    Remove a-key code

--- helpers/config.js
+++ helpers/config.js
@@ -2,5 +2,5 @@ module.exports = {
     TWILIO_AUTH_TOKEN : '',
     YELP_CLIENT_ID: '',
     YELP_CLIENT_SECRET:'',
-    YELP_ACCESS_TOKEN: 'niqmDIRAiQ3MuS8NnTTI6VVjrFc86m-82aoqsgCPogfCnP4xEkJBHF-yxMWRFd75Bpv2AHah-EjJJNuCzZi1NmB6BVVmodgnYNNUyPDppzS4h8nZ4j7sU7kl0vMRWnYx',
+    YELP_ACCESS_TOKEN: '',
 };
